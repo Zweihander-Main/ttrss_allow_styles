5a01c1a7b183bf5b030a132b42a053bc347fce5c
Zweihänder <zweidev@zweihander.me>
2024-01-31 23:06:38 -0500

    fix(init): format

--- init.php
+++ init.php
@@ -1,32 +1,39 @@
 <?php
-class TTRSS_Allow_Styles extends Plugin {
-    function about() {
-        return array(
-            "1.0.0",
-            "Allow styles on selected feeds",
-            "zwei");
-    }
+class TTRSS_Allow_Styles extends Plugin
+{
+	function about()
+	{
+		return array(
+			"1.0.0",
+			"Allow styles on selected feeds",
+			"zwei"
+		);
+	}
 
-	public function api_version() {
+	public function api_version()
+	{
 		return 2;
 	}
 
 	private $host;
 
-	public function init( $host ) {
+	public function init($host)
+	{
 		$this->host = $host;
 
-		$host->add_hook( $host::HOOK_SANITIZE, $this );
+		$host->add_hook($host::HOOK_SANITIZE, $this);
+		$host->add_hook($host::HOOK_PREFS_EDIT_FEED, $this);
 	}
 
-	// public function hook_article_export_feed( $line, $feed, $is_cat ) {
-	// 	if ( ! in_array( $this, $this->host->get_hooks( $this->host::HOOK_SANITIZE ) ) )
-	// 		$this->host->add_hook( $this->host::HOOK_SANITIZE, $this );
+	public function hook_sanitize($doc, $site_url, $allowed_elements, $disallowed_attributes, $article_id)
+	{
 
-	// 	return $line;
-	// }
+		$disallowed_attributes = array_filter($disallowed_attributes, function ($attr) {
+			return $attr !== 'style';
+		});
 
-	public function hook_sanitize( $doc, $site_url, $allowed_elements, $disallowed_attributes, $article_id ) {
+		return [$doc, $allowed_elements, $disallowed_attributes];
+	}
 
 		$disallowed_attributes = array_filter( $disallowed_attributes, function( $attr ) {
 			return $attr !== 'style';
